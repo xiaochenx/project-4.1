14410504457f317329aa8107ed481fb8c0c59bd9
Xiaochen Xu <xiaochex@gmail.com>
2021-07-19 18:20:03 -0400

    added users sessions controllers

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,7 @@
 class Trip < ApplicationRecord
+    validates :title, presence: :true
+    validates :content, presence: :true
+    validates :date, presence: :true
+
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :username, presence: :true
+    validates :username, uniqueness: { case_sensitive: true }
+    
+    has_secure_password
+    
+    has_many :trips
 end
